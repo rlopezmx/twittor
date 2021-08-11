82a66e79a34007b3957ee44cca91c63b9bfeb0eb
rlopezmx <rlopezmx@hotmail.com>
2021-08-11 16:57:30 -0600

    first commit

---
+++ js/sw-utils.js
@@ -0,0 +1,25 @@
+
+
+function actualizaCacheDinamico(dynamicCache, req, res){
+
+
+    if(res.ok) {
+
+        caches.open( dynamicCache).then(cache => {
+
+
+            cache.put( req, res.clone());
+
+            return res.clone();
+
+        });
+    } else {
+
+
+        return  res;
+
+    }
+
+
+
+}
